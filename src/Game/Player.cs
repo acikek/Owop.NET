f51a1f91533bf0498309659bf6ceb14747ac29f1
Skye Prince <spiralixp@gmail.com>
2023-12-07 21:42:01 -0800

    player rank/tool docs

--- src/Game/Player.cs
+++ src/Game/Player.cs
@@ -3,27 +3,61 @@ using System.Numerics;
 
 namespace Owop;
 
+/// <summary>A player's permission rank within a world.</summary>
 public enum PlayerRank
 {
+    /// <summary>No permissions.</summary>
     None,
+    /// <summary>Normal placement permissions.</summary>
     Player,
+    /// <summary>Elevated placement and tool permissions.</summary>
     Moderator,
+    /// <summary>Top-level administrative permissions.</summary>
     Admin
 }
 
+/// <summary>A player's selected tool.</summary>
 public enum PlayerTool
 {
+    /// <summary>Default cursor. Places one pixel at a time.</summary>
     Cursor,
+    /// <summary>View panning tool.</summary>
     Move,
+    /// <summary>Color selection tool.</summary>
     Pipette,
+    /// <summary>
+    /// <b>Moderator-only</b>. 
+    /// Erases an entire chunk, setting its pixels to <see cref="Color.White"/>.
+    /// </summary>
     Eraser,
+    /// <summary>Changes the view resolution.</summary>
     Zoom,
+    /// <summary>Canvas screenshotting tool.</summary>
     Export,
+    /// <summary>Continuously replaces adjacent pixels of a certain color.</summary>
     Fill,
+    /// <summary>Places pixels in a rasterized line.</summary>
     Line,
+    /// <summary>
+    /// <b>Moderator-only.</b>
+    /// Protects a chunk, meaning that only players ranked <see cref="PlayerRank.Moderator"/>
+    /// or above will be able to modify it.
+    /// </summary>
     Protect,
+    /// <summary>
+    /// <b>Moderator-only.</b>
+    /// Protects a set of chunks in some rectangular bounds.
+    /// </summary>
     AreaProtect,
+    /// <summary>
+    /// <b>Moderator-only.</b>
+    /// Pastes an image onto the canvas.
+    /// </summary>
     Paste,
+    /// <summary>
+    /// <b>Moderator-only.</b>
+    /// Copies a section of the canvas.
+    /// </summary>
     Copy
 }
 
