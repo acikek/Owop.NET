5aaf6e205c4e9242cec46ee4e66cafdfb37c28fd
acikek <spiralixp@gmail.com>
2024-01-22 00:41:11 -0800

    didn't save

--- src/World/Chunks/IWorldChunks.cs
+++ src/World/Chunks/IWorldChunks.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ public interface IWorldChunks : IReadOnlyDictionary<Position, IChunk>
     bool IsChunkLoaded(Position chunkPos);
 
     bool IsChunkLoaded(Position chunkPos, out IChunk? chunk);
+
+    Color? GetPixel(Position worldPos);
 
     Task Request(Position chunkPos, bool force = false);
 
