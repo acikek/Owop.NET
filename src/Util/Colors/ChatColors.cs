b26447b84aba11179f28a0915eddd0a5402c2f63
Skye Prince <spiralixp@gmail.com>
2024-04-26 16:55:26 -0700

    color docs

--- src/Util/Colors/ChatColors.cs
+++ src/Util/Colors/ChatColors.cs
@@ -7,12 +7,24 @@ using System.Threading.Tasks;
 
 namespace Owop.Util;
 
+/// <summary>Contains colors used in the OWOP chat interface.</summary>
 public static class ChatColors
 {
+    /// <summary>The color of normal players' message headers.</summary>
     public static readonly Color Nickname = Color.FromArgb(58, 178, 255);
+
+    /// <summary>The color of moderators' messages.</summary>
     public static readonly Color Moderator = Color.FromArgb(134, 255, 65);
+
+    /// <summary>The color of admins' messages.</summary>
     public static readonly Color Admin = Color.FromArgb(255, 79, 79);
+
+    /// <summary>The color of messages relayed from Discord.</summary>
     public static readonly Color Discord = Color.FromArgb(108, 255, 231);
+
+    /// <summary>The color of server messages.</summary>
     public static readonly Color Server = Color.FromArgb(255, 65, 228);
+
+    /// <summary>The color of private messages.</summary>
     public static readonly Color Tell = Color.FromArgb(255, 183, 53);
 }
--- src/Util/Colors/OwopColors.cs
+++ src/Util/Colors/OwopColors.cs
@@ -7,23 +7,57 @@ using System.Threading.Tasks;
 
 namespace Owop.Util;
 
-// ENDESGA 16 Palette
+/// <summary>
+/// Contains colors used in the default OWOP client.
+/// All colors are from the <a href="https://lospec.com/palette-list/endesga-16">ENDSEGA 16</a> palette.
+/// </summary>
 public static class OwopColors
 {
+    /// <summary>A color with an RGB value of <c>#E4A572</c>.</summary>
     public static readonly Color Sand = Color.FromArgb(228, 166, 114);
+
+    /// <summary>A color with an RGB value of <c>#B86F50</c>.</summary>
     public static readonly Color Tan = Color.FromArgb(184, 111, 80);
+
+    /// <summary>A color with an RGB value of <c>#743F39</c>.</summary>
     public static readonly Color Brown = Color.FromArgb(116, 63, 57);
+
+    /// <summary>A color with an RGB value of <c>#3F2832</c>.</summary>
     public static readonly Color DarkBrown = Color.FromArgb(63, 40, 50);
+
+    /// <summary>A color with an RGB value of <c>#9E2836</c>.</summary>
     public static readonly Color DarkRed = Color.FromArgb(158, 40, 53);
+
+    /// <summary>A color with an RGB value of <c>#E53B44</c>.</summary>
     public static readonly Color Red = Color.FromArgb(229, 59, 68);
+
+    /// <summary>A color with an RGB value of <c>#FB932B</c>.</summary>
     public static readonly Color Gold = Color.FromArgb(251, 146, 43);
+
+    /// <summary>A color with an RGB value of <c>#FFE762</c>.</summary>
     public static readonly Color Yellow = Color.FromArgb(255, 231, 98);
+
+    /// <summary>A color with an RGB value of <c>#63C64D</c>.</summary>
     public static readonly Color Lime = Color.FromArgb(99, 198, 77);
+
+    /// <summary>A color with an RGB value of <c>#327345</c>.</summary>
     public static readonly Color Green = Color.FromArgb(50, 115, 69);
+
+    /// <summary>A color with an RGB value of <c>#193D3F</c>.</summary>
     public static readonly Color DarkGreen = Color.FromArgb(25, 61, 63);
+
+    /// <summary>A color with an RGB value of <c>#4F6781</c>.</summary>
     public static readonly Color Gray = Color.FromArgb(79, 103, 129);
+
+    /// <summary>A color with an RGB value of <c>#AFBFD2</c>.</summary>
     public static readonly Color LightGray = Color.FromArgb(175, 191, 210);
+
+    /// <summary>A color with an RGB value of <c>#FFFFFF</c>.</summary>
     public static readonly Color White = Color.White;
+
+    /// <summary>A color with an RGB value of <c>#2CE8F4</c>.</summary>
     public static readonly Color Aqua = Color.FromArgb(44, 232, 244);
+
+    /// <summary>A color with an RGB value of <c>#0484D1</c>.</summary>
     public static readonly Color Blue = Color.FromArgb(4, 132, 209);
 }
